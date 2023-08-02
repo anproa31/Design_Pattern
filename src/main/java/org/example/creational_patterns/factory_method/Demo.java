cce9008c02fac288ba5a65bd095921ee625a570a
anproa31 <antoeta31@gmail.com>
2023-08-02 10:49:02 +0700

    factory method pattern

---
+++ src/main/java/org/example/creational_patterns/factory_method/Demo.java
@@ -0,0 +1,26 @@
+package org.example.creational_patterns.factory_method;
+
+import org.example.creational_patterns.factory_method.factory.Dialog;
+import org.example.creational_patterns.factory_method.factory.HtmlDialog;
+import org.example.creational_patterns.factory_method.factory.WindowsDialog;
+
+public class Demo {
+    private static Dialog dialog;
+    public static void main(String[] args) {
+        configure();
+        runBusinessLogic();
+    }
+
+    static void configure() {
+        if (System.getProperty("os.name").equals("Windows 10")) {
+            dialog = new WindowsDialog();
+        } else {
+            dialog = new HtmlDialog();
+        }
+    }
+
+    static void runBusinessLogic() {
+        dialog.renderWindow();
+    }
+
+}
---
+++ src/main/java/org/example/creational_patterns/factory_method/factory/Dialog.java
@@ -0,0 +1,12 @@
+package org.example.creational_patterns.factory_method.factory;
+
+import org.example.creational_patterns.factory_method.buttons.Button;
+
+public abstract class Dialog {
+    public void renderWindow() {
+        Button okButton = createButton();
+        okButton.render();
+    }
+
+    public abstract Button createButton();
+}
